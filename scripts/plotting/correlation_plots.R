521afa8bf2e7e606e6af1069598fe3c0a4f60727
csmythies <chiara.smythies@gmail.com>
2025-09-08 12:28:09 -0700

    mcmc hisse and musscrat changed

--- scripts/plotting/correlation_plots.R
+++ scripts/plotting/correlation_plots.R
@@ -36,3 +36,34 @@ ggplot(combined, aes(x = sparsity, y = avg_size, color = max_branch)) +
        color = "Branchiness",
        title = "Sparsity vs. Avg Size (branchiness color)")
 
+# Overlapping histogram
+
+library(ggplot2)
+library(dplyr)
+
+
+# Keep only types 0, 1, 2 and combine 1 & 2 into 1
+combined <- full_join(flowers,type_df)
+combined <- combined %>%
+  filter(type %in% c(0,1,2)) %>%   # keep relevant types
+  mutate(type_combined = case_when(
+    type %in% c(0,1) ~ 0,          # type 0 & 1 → 0
+    type == 2 ~ 1                   # type 2 → 1
+  ),
+  type_combined = factor(type_combined, levels = c(0,1))  # type 2 drawn first
+  )
+
+
+# Plot with gray overlay
+ggplot(combined, aes(x = flowers, fill = type_combined)) +
+  geom_histogram(bins = 30, color = "black", alpha = 0.75, position = "identity") +
+  scale_fill_manual(values = c("1" = "skyblue", "0" = "#E41A1C")) +
+  labs(
+    x = "Flower number",
+    y = "Individuals",
+    fill = "Inflorescence type",
+    title = "Flower number and inflorescence type"
+  ) +
+  theme_minimal(base_size = 14)
+
+
