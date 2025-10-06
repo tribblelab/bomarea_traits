8a6b8568f2747331f62ed925f0a2f7ae2dc17cab
csmythies <chiara.smythies@gmail.com>
2025-10-06 08:59:24 -0700

    added histogram of elevation v # of flowers (inflorescence type color) and organized

--- scripts/plotting/correlation_plots.R
+++ scripts/plotting/correlation_plots.R
@@ -1,7 +1,16 @@
 library(ggplot2)
+library(dplyr)
+library(ape)
 
-# Just size and sparsity
-combined <- full_join(sparsity_df,size_df)
+setwd("~/Desktop/bomarea_traits/")
+
+######################
+## Plot 2 varibales ##
+######################
+
+combined <- sparsity_dat %>%
+  left_join(size_dat, by = "label") %>%
+  left_join(flowers_dat, by = "label")
 
 
 combined_subset <- combined[combined$sparsity>0,]
@@ -13,30 +22,34 @@ plot(combined_subset$sparsity,
      main = "Sparsity vs. Avg Size",
      pch = 16)
 
-cor(combined_subset$sparsity, combined_subset$avg_size, use = "complete.obs")
-cor(combined_subset$sparsity, combined_subset$avg_size, 
-    method = "spearman", use = "complete.obs")
+# cor(combined_subset$sparsity, combined_subset$avg_size, use = "complete.obs")
+# cor(combined_subset$sparsity, combined_subset$avg_size, 
+    # method = "spearman", use = "complete.obs")
 
-# Color code (elevation,branchiness,type)
+######################
+## Plot 3 varibales ##
+######################
 
-library(dplyr)
+combined <- sparsity_dat %>%
+  left_join(size_dat, by = "label") %>%
+  left_join(flowers_dat, by = "label")%>%
+  left_join(type_dat, by = "label")%>%
+  left_join(sparsity_dat, by = "label")
 
-combined <- sparsity_df %>%
-  full_join(size_df, by = "acceptedName") %>%
-  full_join(elevation_df, by = "acceptedName")%>%
-  full_join(type_df, by = "acceptedName")%>%
-  full_join(branchiness_max, by = "acceptedName")
+# combined$max_branch <- as.factor(combined$max_branch)
 
-combined$max_branch <- as.factor(combined$max_branch)
-
-ggplot(combined, aes(x = sparsity, y = avg_size, color = max_branch)) +
+ggplot(combined, aes(x = flowers, y = sparsity.x, color = type)) +
   geom_point(size = 3) +
-  labs(x = "Sparsity",
+  labs(x = "Number of Flowers",
        y = "Avg Size",
-       color = "Branchiness",
-       title = "Sparsity vs. Avg Size (branchiness color)")
+       color = "Inflorescence Type",
+       title = "# of Flowers vs. Avg Size (inflorescence color)")
 
-# Overlapping histogram
+
+
+###############################################################################
+## Histogram of flower number vs # of individuals (inflorescence type color) ##
+###############################################################################
 
 library(ggplot2)
 library(dplyr)
@@ -65,5 +78,44 @@ ggplot(combined, aes(x = flowers, fill = type_combined)) +
     title = "Flower number and inflorescence type"
   ) +
   theme_minimal(base_size = 14)
+
+
+
+
+##############################################################
+## Histogram Elevation vs # of species (inflorescence color ##
+##############################################################
+
+library(dplyr)
+library(ape)
+setwd("~/Desktop/bomarea_traits/")
+
+merged <- left_join(elevation_dat, type_dat, by = "label")
+
+# Label the elevation bins
+merged$elevation_label <- recode_factor(
+  as.character(merged$elevation_bin),
+  "0" = "0–1000",
+  "1" = "1000–2500",
+  "2" = "2500–4500",
+  "3" = "4500+"
+)
+
+# Summarize counts per elevation zone and inflorescence type
+summary_df <- combined %>%
+  group_by(elevation_label, type) %>%
+  summarise(species_count = n())
+
+# Bar plot
+ggplot(merged, aes(x = elevation_label, fill = type)) +
+  geom_bar(position = "dodge", color = "black") +
+  labs(
+    title = "Elevation and # of species (inflorescence type)",
+    x = "Elevation (m)",
+    y = "Number of Species",
+    fill = "Inflorescence Type"
+  ) +
+  theme_classic(base_size = 14)
+
 
 
