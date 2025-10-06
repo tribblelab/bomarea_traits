ff0d96976dd8fb4a72207f72777a37f658d40058
csmythies <chiara.smythies@gmail.com>
2025-10-06 08:59:39 -0700

    plotting the hisse (carries version)

--- scripts/plotting/plot_hisse_CMT.r
+++ scripts/plotting/plot_hisse_CMT.r
@@ -8,7 +8,7 @@ HiSSE_maps_file <- "output/stoch_maps_hisse_rj.log"
 
 # read in data
 p_tree <- readTrees(HiSSE_tree_file)
-p_anc <- processAncStates(HiSSE_file, state_labels = c("0" = "0A", 
+p_anc <- processAncStates(HiSSE_tree_file, state_labels = c("0" = "0A", 
                                                        "1" = "1A", 
                                                        "2" = "0B", 
                                                        "3" = "1B"))
@@ -18,7 +18,20 @@ p_maps <- processStochMaps(tree = p_tree,
                            states = as.character(0:3) )                
 
 # plot the ancestral states
-pies <- plotAncStatesPie(p_anc)
+pies <- plotAncStatesPie(
+  p_anc,
+  tip_labels = TRUE,
+  state_transparency = 1.0,
+  node_pie_size = 1,
+  tip_pie_size = 0.85
+) +
+  theme(
+    legend.text  = element_text(size = 13),
+    legend.title = element_text(size = 15)
+  )
+
+ggsave("~/Desktop/hisse_asr.pdf",
+           height = 10, width = 8)
 
 # plot net div rates
 p_rates[[1]]$net_div_0 <- p_rates[[1]]$`speciation_observed[1]` -  
@@ -49,7 +62,7 @@ map_plot <- plotStochMaps(tree = p_tree,
                          "1" = colFun(4)[3],
                          "2" = colFun(4)[2],
                          "3" = colFun(4)[4]),
-              color_by = "MAP") 
+              color_by = "MAP")
 
 ggsave("~/Desktop/hisse_stoch_maps.pdf",
            height = 10, width = 8)
